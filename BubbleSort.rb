acb3e959186233d7483b4998cb7f93e1023acedc
Brian Yingling <brian@jet.com>
2014-08-16 16:12:24 -0400

    bubble sort in ruby

---
+++ BubbleSort.rb
@@ -0,0 +1,17 @@
+def BubbleSort(nums)
+	swapped = true
+	while swapped
+		swapped = false
+		for i in 0..nums.size-2
+			if nums[i] > nums[i+1]
+				temp = nums[i+1]
+				nums[i+1] = nums[i]
+				nums[i] = temp
+				swapped = true
+			end
+		end
+	end
+	nums
+end
+
+puts BubbleSort([9,8,7,6,5,4,3,2,1]).inspect
