5c7e3828ceced3244efc60599380c68d51be29b4
Brian Yingling <brian@jet.com>
2014-08-16 14:10:20 -0400

    simple insertion sort example

---
+++ InsertionSort.js
@@ -0,0 +1,18 @@
+function InsertionSort(nums) {
+	for (var i=0; i < nums.length; i++) {
+		var current = nums[i];
+		var j = i-1;
+
+		// shift the other numbers over to make way
+		// for current's new slot
+		while (j >= 0 && current < nums[j]) {
+			nums[j+1] = nums[j];
+			j = j-1;
+		}
+		nums[j+1] = current;
+	}
+	return nums;
+}
+
+console.log( InsertionSort([9,8,5,4,2]));
+console.log( InsertionSort([1,3,4,0,1]));
